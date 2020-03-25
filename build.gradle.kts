b289a1619edaae7482bb2fd49945c678e8d607a9
sakebook <sakebook@gmail.com>
2020-03-25 21:13:52 +0900

    Setting use cloud functions

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,15 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-js"))
+    implementation(npm("firebase-admin", "^8.10.0"))
+    implementation(npm("firebase-functions", "^3.5.0"))
 }
 
-kotlin.target.nodejs { }
+kotlin {
+    sourceSets["main"].kotlin.srcDir("src/main/external")
+    target {
+        nodejs {}
+        useCommonJs()
+    }
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
+external val exports: dynamic
+external fun require(module: String): dynamic
+
+val functions = require("firebase-functions")
+
 fun main() {
     println("Hello Kotlin/JS!")
+    exports.message = functions.https.onRequest { req, res ->
+        console.log("message")
+        res.status(200).send("Hello World!")
+    }
 }
