21e8a22545d44cb4f6642a8a061536e0591c51fb
sakebook <sakebook@gmail.com>
2020-03-29 22:56:12 +0900

    Modify build setting for Cloud Functions deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("js") version "1.3.70"
+    kotlin("js") version "1.3.71"
 }
 
 group = "com.github.sakebook"
@@ -13,6 +13,7 @@ dependencies {
     implementation(kotlin("stdlib-js"))
     implementation(npm("firebase-admin", "^8.10.0"))
     implementation(npm("firebase-functions", "^3.5.0"))
+    implementation(npm("@firebase/app", "^0.6.0"))
 }
 
 kotlin {
@@ -21,5 +22,32 @@ kotlin {
         nodejs {}
         useCommonJs()
     }
+}
 
+tasks {
+    val jsRename by creating(Copy::class) {
+        val path = "build/js/packages/${project.name}/kotlin/${project.name}.js"
+        if (File(path).exists()) {
+            from(path)
+            into("functions")
+            rename { it.replace(project.name, "index") }
+        }
+    }
+
+    val depRewrite by creating(Copy::class) {
+        val path = "build/js/packages/${project.name}/package.json"
+        if (File(path).exists()) {
+            from(path)
+            into("functions")
+            val file = File(path)
+            val texts = file.readLines()
+                .filterNot { it.contains("kotlin-source-map-loader") }
+                .map { it.replace("kotlin/${project.name}", "index") }
+            file.writeText(texts.joinToString("\n"))
+        }
+    }
+
+    val cloudFunctions by registering {
+        dependsOn(jsRename, depRewrite)
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,16 @@
 external val exports: dynamic
 external fun require(module: String): dynamic
 
-val functions = require("firebase-functions")
+@JsModule("firebase-functions")
+@JsNonModule
+external object functions {
+    val https: dynamic
+}
 
 fun main() {
     println("Hello Kotlin/JS!")
     exports.message = functions.https.onRequest { req, res ->
         console.log("message")
-        res.status(200).send("Hello World!")
+        res.status(200).send("Hello World!!")
     }
 }
